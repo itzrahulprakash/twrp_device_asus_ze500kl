37e46c861e32d35ae08c75a27d9d891824dbb2ec
Jhyrachy <jacopopieri@gmail.com>
2015-10-13 21:08:20 +0200

    fix

--- omni.mk
+++ omni.mk
@@ -1,11 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := ze500kl
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
-$(call inherit-product, device/asus/ze500kl/device_ze500kl.mk)
+$(call inherit-product, device/asus/ze500kl/omni_ze500kl.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ze500kl
