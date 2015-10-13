eec9db1289082bb6fdb256091fe590550ca417cc
Jhyrachy <jacopopieri@gmail.com>
2015-10-13 21:18:46 +0200

    more fix

--- omni_ze500kl.mk
+++ omni_ze500kl.mk
@@ -6,12 +6,12 @@ $(call inherit-product, device/asus/ze500kl/omni_ze500kl.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ze500kl
-PRODUCT_NAME := cm_ze500kl
+PRODUCT_NAME := omni_ze500kl
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ze500kl
 PRODUCT_MANUFACTURER := asus
 
-DEVICE_PACKAGE_OVERLAYS += device/asus/ze500kl/overlay
+#DEVICE_PACKAGE_OVERLAYS += device/asus/ze500kl/overlay
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
